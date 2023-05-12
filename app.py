857742cd1a46b87cc5a7c11e14d9d37d088b6bff
MyeongSeok11 <103018497+MyeongSeok98@users.noreply.github.com>
2023-05-12 13:37:53 +0900

    수정

--- app.py
+++ app.py
@@ -1,21 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import tensorflow as tf
+import cv2
+import  numpy as np
+import json
 
+# 플라스크 시작
 app = Flask(__name__)
 
-student_data = {
-    1: {"name": "노무현", "score": {"국어": 5, "수학":  23}},
-    2: {"name": "전두환", "score": {"국어": 5, "수학":  18}}
-}
+model = tf.keras.models.load_model('model/model.h5')
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+path = './image/Zzim.jpg'
+with open(path, 'rb') as f:
+	data = f.read()
+
+# 루트 파일
 @app.route('/')
 def index():
-    return render_template("index.html",
-                           template_students = student_data)
+	return 'Hello Data Science Optimizers'
 
-@app.route('/student/<int:id>')
-def user(id):
-    return render_template("student.html",
-                           template_students = student_data[id]["name"],
-                           template_score=student_data[id]["score"])
+# HTML 파일 추가
+@app.route('/home')
+def home():
+	return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET'])
+def predict():
+	# 이미지 읽어옴
+	img = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_UNCHANGED)
+
+	# 이미지 416 416으로 리사이징 시킴
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	img = cv2.resize(img, (416, 416))
+	img = img / 255.0
+	img = np.expand_dims(img, axis=0)
+
+	# 예측 수행
+	predictions = model.predict(img)
+
+	# 클래스, 확률, x, y, w, h 추출
+	results = []
+	for output in predictions:
+		for detection in output:
+			obj = {
+				"class": int(detection[4]),
+				"confidence": float(detection[5]),
+				"x": int(detection[0]),
+				"y": int(detection[1]),
+				"w": int(detection[2] - detection[0]),
+				"h": int(detection[3] - detection[1])
+			}
+			results.append(obj)
+
+	# json으로 result 넘김
+	return json.dumps(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
